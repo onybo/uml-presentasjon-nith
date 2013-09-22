e6081fc2abd17fa3200b5d9f0ead94cae0ffe7fd
olavnybo <olavnybo@gmail.com>
2013-09-22 20:58:19 +0200

    initial commit

---
+++ ModelingProject1/ClassLibrary1/ClassA.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class ClassA
+    {
+        private ClassB _b;
+        public ClassA(ClassB b)
+        {
+            _b = b;
+        }
+
+        public void DoSomething()
+        {
+            int sum = 0;
+            for (var i = 0; i < 10; i++)
+            {
+                sum += _b.MethodB();                
+            }
+            Console.WriteLine("sum er :" + sum);
+        }
+    }
+}
